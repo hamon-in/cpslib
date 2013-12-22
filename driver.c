e8b30823e49ab361cfcdf1513b493f0028ae229a
Noufal Ibrahim <noufal@nibrahim.net.in>
2013-12-23 02:21:40 +0530

    Adds driver code for disk_partitions.
    Signed-off-by: Noufal Ibrahim <noufal@nibrahim.net.in>

--- driver.c
+++ driver.c
@@ -4,11 +4,25 @@
 int
 main()
 {
+  int i;
   DiskUsage du;
+  DiskPartitionInfo dp;
+  printf(" Disk usage \n");
   disk_usage("/", &du);
   printf("total: %ld\nused: %ld\nfree: %ld\npercent: %f\n", du.total, du.used, du.free, du.percent);
+
+  printf(" Disk partitions \n");
+  disk_partitions(&dp);
+  printf("Partitions : %d\n", dp.nitems);
+  for(i = 0; i < dp.nitems; i++)
+    printf("%s %s %s %s\n", 
+           dp.partitions[i].device,
+           dp.partitions[i].mountpoint,
+           dp.partitions[i].fstype,
+           dp.partitions[i].opts);
+    
+  free_disk_partition_info(&dp);
   return 0;
 }
-
 
 
