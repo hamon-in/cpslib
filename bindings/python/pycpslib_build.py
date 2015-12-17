7edbb120fd84f3a40e6acedc81e06073bb8f5f62
Noufal Ibrahim <noufal@nibrahim.net.in>
2015-12-17 14:46:52 +0530

    Adds skeleton to create cpslib python bindings

---
+++ bindings/python/pycpslib_build.py
@@ -0,0 +1,39 @@
+# pycpslib.py
+
+import os
+
+from cffi import FFI
+
+ffi = FFI()
+project_root = os.path.abspath("../../")
+
+ffi.set_source("pycpslib",
+               """#include <stdio.h>
+               #include <stdlib.h>
+               #include <sys/types.h>
+               #include <unistd.h>
+               #include "pslib.h"
+               """,
+               libraries = ["pslib"],
+               library_dirs = [project_root],
+               include_dirs = [project_root])
+
+
+ffi.cdef("""
+typedef struct {
+  char *device;
+  char *mountpoint;
+  char *fstype;
+  char *opts;
+} DiskPartition; /* TBD: Pluralise */
+
+typedef struct {
+  int nitems;
+  DiskPartition *partitions;
+} DiskPartitionInfo;
+
+DiskPartitionInfo *disk_partitions(int);
+""")
+
+if __name__ == '__main__':
+    ffi.compile()
