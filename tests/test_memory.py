9bf1f75ac7ccfb11ce29437b6e8db9c619a56ed9
Noufal Ibrahim <noufal@nibrahim.net.in>
2015-12-21 00:51:29 +0530

    Allows for tolerance in comparing floats

--- tests/test_memory.py
+++ tests/test_memory.py
@@ -2,18 +2,17 @@ import psutil
 from pycpslib import lib as P 
 from pycpslib import ffi
 
-def test_virtual_memory():
+def test_virtual_memory(almost_equal):
     pslib_vmem = ffi.new("VmemInfo *")
     P.virtual_memory(pslib_vmem)
-
     psutil_vmem = psutil.virtual_memory()
-    
-    assert pslib_vmem.total == psutil_vmem.total
-    assert pslib_vmem.available == psutil_vmem.available
-    assert pslib_vmem.used == psutil_vmem.used
-    assert pslib_vmem.free == psutil_vmem.free
-    assert pslib_vmem.active == psutil_vmem.active
-    assert pslib_vmem.inactive == psutil_vmem.inactive
-    assert pslib_vmem.buffers == psutil_vmem.buffers
-    assert pslib_vmem.cached == psutil_vmem.cached
-    assert pslib_vmem.percent == psutil_vmem.percent
+
+    assert almost_equal(pslib_vmem.total, psutil_vmem.total)
+    assert almost_equal(pslib_vmem.available, psutil_vmem.available)
+    assert almost_equal(pslib_vmem.used, psutil_vmem.used)
+    assert almost_equal(pslib_vmem.free, psutil_vmem.free)
+    assert almost_equal(pslib_vmem.active, psutil_vmem.active)
+    assert almost_equal(pslib_vmem.inactive, psutil_vmem.inactive)
+    assert almost_equal(pslib_vmem.buffers, psutil_vmem.buffers)
+    assert almost_equal(pslib_vmem.cached, psutil_vmem.cached)
+    assert almost_equal(pslib_vmem.percent, psutil_vmem.percent)
