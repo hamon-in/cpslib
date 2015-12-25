f1d6848ed78cfb3070a0f993f5607f941d11aa7e
Kartik Singhal <kartiksinghal@gmail.com>
2015-12-25 17:48:35 -0500

    Add cpu_percent (cpu_util_percent) and cpu_times_percent

--- driver.c
+++ driver.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "pslib.h"
+
+void test_getusers() {
+  UsersInfo *r;
+  int i;
+  printf(" -- users \n");
+  r = get_users();
+  if (!r) {
+    printf("Failed \n");
+    return;
+  }
+  printf("Total: %d\n", r->nitems);
+  printf("Name\tTerminal Host\tStarted\n");
+  for (i = 0; i < r->nitems; i++) {
+    printf("%s\t%s\t %s\t%.1f\n", r->users[i].username, r->users[i].tty,
+           r->users[i].hostname, r->users[i].tstamp);
+  }
+  free_users_info(r);
+  printf("\n");
+}
 
 void test_boottime() {
   float t = get_boot_time();
-  printf(" Boot time \n");
+  printf(" -- boot_time \n");
   printf("%.1f\n\n", t);
-}
-
-void test_cpu_count() {
-  int logical;
-  int physical;
-  logical = cpu_count(1);
-  physical = cpu_count(0);
-  printf(" CPU count \n");
-  if (logical == -1 || physical == -1) {
-    printf("Aborting\n");
-    return;
-  }
-  printf("Logical : %d\nPhysical : %d\n", logical, physical);
-  printf("\n");
 }
 
 void test_cpu_times() {
@@ -29,7 +35,7 @@ void test_cpu_times() {
     printf("Aborting\n");
     return;
   }
-  printf(" CPU times\n");
+  printf(" -- cpu_times\n");
   printf("User: %.3lf;", r->user);
   printf(" Nice: %.3lf;", r->nice);
   printf(" System: %.3lf;", r->system);
@@ -47,7 +53,7 @@ void test_cpu_times_percpu() {
     printf("Aborting\n");
     return;
   }
-  printf(" Individual CPU times\n");
+  printf(" -- cpu_times_percpu\n");
   for (i = 0; i < ncpus; i++) {
     printf("CPU %d :: ", i + 1);
     printf(" Usr: %.3lf;", c->user);
@@ -61,22 +67,130 @@ void test_cpu_times_percpu() {
   free(r);
 }
 
-void test_getusers() {
-  UsersInfo *r;
-  int i;
-  printf(" Logged in users \n");
-  r = get_users();
-  if (!r) {
-    printf("Failed \n");
+void test_cpu_util_percent() {
+  CpuTimes *info;
+  double *utilisation;
+  info = cpu_times(0);
+
+  if (!info) {
+    printf("Aborting\n");
     return;
   }
-  printf("Total: %d\n", r->nitems);
-  printf("Name\tTerminal Host\tStarted\n");
-  for (i = 0; i < r->nitems; i++) {
-    printf("%s\t%s\t %s\t%.1f\n", r->users[i].username, r->users[i].tty,
-           r->users[i].hostname, r->users[i].tstamp);
+
+  usleep(100000);
+  utilisation = cpu_util_percent(0, info);
+  printf(" -- cpu_util_percent\n");
+  printf("%f\n", *utilisation);
+  printf("\n");
+
+  free(utilisation);
+  free(info);
+}
+
+void test_cpu_util_percent_percpu() {
+  CpuTimes *info;
+  double *percentages;
+  int i, ncpus = cpu_count(1);
+  info = cpu_times(1);
+
+  if (!info) {
+    printf("Aborting\n");
+    return;
   }
-  free_users_info(r);
+
+  usleep(100000);
+  percentages = cpu_util_percent(1, info);
+  printf(" -- cpu_util_percent_percpu\n");
+  for (i = 0; i < ncpus; i++) {
+    printf("Cpu #%d : %f\n", i, percentages[i]);
+  }
+
+  printf("\n");
+
+  free(percentages);
+  free(info);
+}
+
+void test_cpu_times_percent() {
+  CpuTimes *info;
+  CpuTimes *ret;
+  info = cpu_times(0);
+  if (!info) {
+    printf("Aborting\n");
+    return;
+  }
+  usleep(100000);
+  ret = cpu_times_percent(0, info); /* Actual percentages since last call */
+  if (!ret) {
+    printf("Error while computing utilisation percentage\n");
+    return;
+  }
+  printf(" -- cpu_times_percent\n");
+  printf("CPU times as percentage of total (0.1 second sample)\n");
+  printf("Usr: %.3lf;", ret->user);
+  printf(" Nice: %.3lf;", ret->nice);
+  printf(" Sys: %.3lf;", ret->system);
+  printf(" Idle: %.3lf;", ret->idle);
+  printf(" IOWait: %.3lf;", ret->iowait);
+  printf(" IRQ: %.3lf;", ret->irq);
+  printf(" SoftIRQ: %.3lf;", ret->softirq);
+  printf(" Steal: %.3lf;", ret->steal);
+  printf(" Guest: %.3lf;", ret->guest);
+  printf(" Guest nice: %.3lf\n", ret->guest_nice);
+  printf("\n");
+  free(info);
+  free(ret);
+}
+
+void test_cpu_times_percent_percpu() {
+  CpuTimes *info, *last, *r;
+  int i, ncpus = cpu_count(1);
+  last = cpu_times(1);
+  if (!last) {
+    printf("Aborting\n");
+    return;
+  }
+  usleep(100000);
+  r = info =
+      cpu_times_percent(1, last); /* Actual percentages since last call */
+  if (!info) {
+    printf("Error while computing utilisation percentage\n");
+    return;
+  }
+
+  printf(" -- cpu_times_percent_percpu\n");
+  printf("CPU times as percentage of total per CPU (0.1 second sample)\n");
+  for (i = 0; i < ncpus; i++) {
+    printf("CPU %d :: ", i + 1);
+    printf("Usr: %.3lf;", info->user);
+    printf(" Nice: %.3lf;", info->nice);
+    printf(" Sys: %.3lf;", info->system);
+    printf(" Idle: %.3lf;", info->idle);
+    printf(" IOWait: %.3lf;", info->iowait);
+    printf(" IRQ: %.3lf;", info->irq);
+    printf(" SoftIRQ: %.3lf;", info->softirq);
+    printf(" Steal: %.3lf;", info->steal);
+    printf(" Guest: %.3lf;", info->guest);
+    printf(" Guest nice: %.3lf\n", info->guest_nice);
+    info++;
+  }
+
+  printf("\n");
+  free(r);
+  free(last);
+}
+
+void test_cpu_count() {
+  int logical;
+  int physical;
+  logical = cpu_count(1);
+  physical = cpu_count(0);
+  printf(" -- cpu_count \n");
+  if (logical == -1 || physical == -1) {
+    printf("Aborting\n");
+    return;
+  }
+  printf("Logical : %d\nPhysical : %d\n", logical, physical);
   printf("\n");
 }
 
@@ -92,12 +206,12 @@ int main() {
   //
   test_cpu_times();
   test_cpu_times_percpu();
-  //
-  //  test_cpu_util_percent();
-  //  test_cpu_util_percent_percpu();
-  //
-  //  test_cpu_times_percent();
-  //  test_cpu_times_percent_percpu();
+
+  test_cpu_util_percent();
+  test_cpu_util_percent_percpu();
+
+  test_cpu_times_percent();
+  test_cpu_times_percent_percpu();
   //
   test_cpu_count();
   //  test_process();
--- pslib_osx.c
+++ pslib_osx.c
@@ -12,77 +12,7 @@
  * something */
 
 /* Internal functions */
-CpuTimes *per_cpu_times();
-
-/* Public functions */
-
-float get_boot_time() {
-  /* read KERN_BOOTIME */
-  int mib[2] = {CTL_KERN, KERN_BOOTTIME};
-  struct timeval result;
-  size_t len = sizeof result;
-  time_t boot_time = 0;
-
-  check(sysctl(mib, 2, &result, &len, NULL, 0) != -1, "sysctl failed");
-  boot_time = result.tv_sec;
-  return (float)boot_time;
-
-error:
-  return -1;
-}
-
-int cpu_count(int logical) {
-  int ncpu;
-  size_t len = sizeof(ncpu);
-
-  if (logical) {
-    check(sysctlbyname("hw.logicalcpu", &ncpu, &len, NULL, 0) != -1,
-          "sysctl failed");
-  } else {
-    check(sysctlbyname("hw.physicalcpu", &ncpu, &len, NULL, 0) != -1,
-          "sysctl failed");
-  }
-
-  return ncpu;
-
-error:
-  return -1;
-}
-
-CpuTimes *cpu_times(int percpu) {
-  CpuTimes *ret = NULL;
-
-  if (!percpu) {
-    ret = (CpuTimes *)calloc(1, sizeof(CpuTimes));
-
-    mach_msg_type_number_t count = HOST_CPU_LOAD_INFO_COUNT;
-    kern_return_t kerror;
-    host_cpu_load_info_data_t r_load;
-
-    mach_port_t host_port = mach_host_self();
-    kerror = host_statistics(host_port, HOST_CPU_LOAD_INFO,
-                             (host_info_t)&r_load, &count);
-    check(kerror == KERN_SUCCESS, "Error in host_statistics(): %s",
-          mach_error_string(kerror));
-    mach_port_deallocate(mach_task_self(), host_port);
-
-    ret->user = (double)r_load.cpu_ticks[CPU_STATE_USER] / CLK_TCK;
-    ret->nice = (double)r_load.cpu_ticks[CPU_STATE_NICE] / CLK_TCK;
-    ret->system = (double)r_load.cpu_ticks[CPU_STATE_SYSTEM] / CLK_TCK;
-    ret->idle = (double)r_load.cpu_ticks[CPU_STATE_IDLE] / CLK_TCK;
-
-    return ret;
-  } else {
-    return per_cpu_times();
-  }
-
-error:
-  if (ret)
-    free(ret);
-  return NULL;
-}
-
-CpuTimes *per_cpu_times() {
+static CpuTimes *per_cpu_times() {
   CpuTimes *ret = NULL;
 
   natural_t cpu_count;
@@ -132,6 +62,173 @@ error:
       log_warn("vm_deallocate() failed");
   }
   return NULL;
+}
+
+static double sum_cpu_time(CpuTimes *t) {
+  double ret = 0.0;
+  ret += t->user;
+  ret += t->system;
+  ret += t->idle;
+  ret += t->nice;
+  ret += t->iowait;
+  ret += t->irq;
+  ret += t->softirq;
+  ret += t->steal;
+  ret += t->guest;
+  ret += t->guest_nice;
+  return ret;
+}
+
+static CpuTimes *calculate_cpu_times_percentage(CpuTimes *t1, CpuTimes *t2) {
+  CpuTimes *ret;
+  double all_delta = sum_cpu_time(t2) - sum_cpu_time(t1);
+  ret = (CpuTimes *)calloc(1, sizeof(CpuTimes));
+  check_mem(ret);
+
+  ret->user = 100 * (t2->user - t1->user) / all_delta;
+  ret->system = 100 * (t2->system - t1->system) / all_delta;
+  ret->idle = 100 * (t2->idle - t1->idle) / all_delta;
+  ret->nice = 100 * (t2->nice - t1->nice) / all_delta;
+  ret->iowait = 100 * (t2->iowait - t1->iowait) / all_delta;
+  ret->irq = 100 * (t2->irq - t1->irq) / all_delta;
+  ret->softirq = 100 * (t2->softirq - t1->softirq) / all_delta;
+  ret->steal = 100 * (t2->steal - t1->steal) / all_delta;
+  ret->guest = 100 * (t2->guest - t1->guest) / all_delta;
+  ret->guest_nice = 100 * (t2->guest_nice - t1->guest_nice) / all_delta;
+  return ret;
+
+error:
+  if (ret)
+    free(ret);
+  return NULL;
+}
+
+static double calculate_cpu_util_percentage(CpuTimes *t1, CpuTimes *t2) {
+  double t1_all = sum_cpu_time(t1);
+  double t2_all = sum_cpu_time(t2);
+  double t1_busy = t1_all - t1->idle;
+  double t2_busy = t2_all - t2->idle;
+  double busy_delta, all_delta, busy_percentage;
+
+  /* This exists in psutils. We'll put it in if we actually find it */
+  /* if (t2_busy < t1_busy)  */
+  /*   return 0.0; /\* Indicates a precision problem *\/ */
+
+  busy_delta = t2_busy - t1_busy;
+  all_delta = t2_all - t1_all;
+  busy_percentage = (busy_delta / all_delta) * 100;
+  return busy_percentage;
+}
+
+/* Public functions */
+
+float get_boot_time() {
+  /* read KERN_BOOTIME */
+  int mib[2] = {CTL_KERN, KERN_BOOTTIME};
+  struct timeval result;
+  size_t len = sizeof result;
+  time_t boot_time = 0;
+
+  check(sysctl(mib, 2, &result, &len, NULL, 0) != -1, "sysctl failed");
+  boot_time = result.tv_sec;
+  return (float)boot_time;
+
+error:
+  return -1;
+}
+
+CpuTimes *cpu_times(int percpu) {
+  CpuTimes *ret = NULL;
+
+  if (!percpu) {
+    ret = (CpuTimes *)calloc(1, sizeof(CpuTimes));
+
+    mach_msg_type_number_t count = HOST_CPU_LOAD_INFO_COUNT;
+    kern_return_t kerror;
+    host_cpu_load_info_data_t r_load;
+
+    mach_port_t host_port = mach_host_self();
+    kerror = host_statistics(host_port, HOST_CPU_LOAD_INFO,
+                             (host_info_t)&r_load, &count);
+    check(kerror == KERN_SUCCESS, "Error in host_statistics(): %s",
+          mach_error_string(kerror));
+    mach_port_deallocate(mach_task_self(), host_port);
+
+    ret->user = (double)r_load.cpu_ticks[CPU_STATE_USER] / CLK_TCK;
+    ret->nice = (double)r_load.cpu_ticks[CPU_STATE_NICE] / CLK_TCK;
+    ret->system = (double)r_load.cpu_ticks[CPU_STATE_SYSTEM] / CLK_TCK;
+    ret->idle = (double)r_load.cpu_ticks[CPU_STATE_IDLE] / CLK_TCK;
+
+    return ret;
+  } else {
+    return per_cpu_times();
+  }
+
+error:
+  if (ret)
+    free(ret);
+  return NULL;
+}
+
+double *cpu_util_percent(int percpu, CpuTimes *prev_times) {
+  CpuTimes *current = NULL;
+  int i, ncpus = percpu ? cpu_count(1) : 1;
+  double *percentage = (double *)calloc(ncpus, sizeof(double));
+  check(prev_times, "Need a reference point. prev_times can't be NULL");
+
+  current = cpu_times(percpu);
+  check(current, "Couldn't obtain CPU times");
+
+  for (i = 0; i < ncpus; i++) {
+    percentage[i] = calculate_cpu_util_percentage(prev_times + i, current + i);
+  }
+  free(current);
+  return percentage;
+error:
+  if (current)
+    free(current);
+  return NULL;
+}
+
+CpuTimes *cpu_times_percent(int percpu, CpuTimes *prev_times) {
+  CpuTimes *current = NULL;
+  CpuTimes *t;
+  int i, ncpus = percpu ? cpu_count(1) : 1;
+  CpuTimes *ret;
+  check(prev_times, "Need a reference point. prev_times can't be NULL");
+  current = cpu_times(percpu);
+  check(current, "Couldn't obtain CPU times");
+  ret = (CpuTimes *)calloc(ncpus, sizeof(CpuTimes));
+  check_mem(ret);
+  for (i = 0; i < ncpus; i++) {
+    t = calculate_cpu_times_percentage(prev_times + i, current + i);
+    *(ret + i) = *t;
+    free(t);
+  }
+  free(current);
+  return ret;
+error:
+  if (current)
+    free(current);
+  return NULL;
+}
+
+int cpu_count(int logical) {
+  int ncpu;
+  size_t len = sizeof(ncpu);
+
+  if (logical) {
+    check(sysctlbyname("hw.logicalcpu", &ncpu, &len, NULL, 0) != -1,
+          "sysctl failed");
+  } else {
+    check(sysctlbyname("hw.physicalcpu", &ncpu, &len, NULL, 0) != -1,
+          "sysctl failed");
+  }
+
+  return ncpu;
+
+error:
+  return -1;
 }
 
 UsersInfo *get_users() {
@@ -184,6 +281,8 @@ error:
   free_users_info(ret);
   return NULL;
 }
+
+/* Auxiliary public functions for garbage collection */
 
 void free_users_info(UsersInfo *ui) {
   Users *u = ui->users;
