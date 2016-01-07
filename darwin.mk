02d7db5856d381509ff394e8361a9e30d6558bc4
Kartik Singhal <kartiksinghal@gmail.com>
2016-01-06 23:44:55 -0500

    Smarten up Makefile to choose target platform

---
+++ darwin.mk
@@ -0,0 +1,36 @@
+CFLAGS  = -fPIC -Wall -Werror -Wunused -Wextra -O0 -g -std=gnu11
+CFLAGS += -fprofile-arcs -ftest-coverage
+LDFLAGS  = -dynamiclib --coverage
+LDFLAGS += -framework CoreFoundation -framework IOKit
+RM = rm -rf
+TARGET_LIB = libpslib.dylib
+EXEC = driver
+
+SRCS = common.c pslib_osx.c
+OBJS = $(SRCS:.c=.o)
+
+.PHONY: all
+all: $(EXEC)
+
+.PHONY: shared
+shared: $(TARGET_LIB)
+
+$(TARGET_LIB): $(OBJS)
+	$(CC) ${LDFLAGS} -o $@ $^
+
+$(EXEC): $(EXEC).c $(TARGET_LIB)
+	$(CC) ${CFLAGS} -o $@ $< -L. -lpslib -Wl,-rpath .
+
+test: clean shared
+	cd bindings/python && make clean && make && cd - && export DYLD_LIBRARY_PATH=`pwd` && py.test -v -ra
+
+.PHONY: covclean
+covclean:
+	${RM} *.gcno *.gcda *.gcov
+
+.PHONY: clean
+clean: covclean
+	${RM} ${TARGET_LIB} ${OBJS} $(EXEC) *dSYM
+
+check-syntax:
+	gcc -Wall -o /dev/null -S ${CHK_SOURCES}
