c7b9ea0bb137d35d5246fdde7fb394e1fd39a3cf
Kartik Singhal <kartiksinghal@gmail.com>
2016-01-11 23:25:50 -0500

    Exclude #include in CFFI cdef

--- bindings/python/pycpslib_build.py
+++ bindings/python/pycpslib_build.py
@@ -20,9 +20,14 @@ ffi.set_source("pycpslib",
 
 ffi.cdef('''
 typedef int32_t pid_t;
+typedef int32_t bool;
 ''')
 
-ffi.cdef(open(project_root+"/pslib.h").read())
+lines = open(project_root+"/pslib.h").readlines()
+
+altered_lines = ['' if line.startswith('#include ') else line for line in lines]
+
+ffi.cdef(''.join(altered_lines))
 
 if __name__ == '__main__':
     ffi.compile()
