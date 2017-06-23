bad8467598e3bc1455a91f0d6cf4a0a327c846fc
libin-n-g <libinngeorge@gmail.com>
2017-06-23 10:16:45 +0530

    Add disk_partitions

--- pslib_windows.c
+++ pslib_windows.c
@@ -25,6 +25,19 @@ typedef struct _DISK_PERFORMANCE_WIN_2008 {
 	WCHAR         StorageManagerName[8];
 } DISK_PERFORMANCE_WIN_2008;
 
+typedef struct _WINSTATION_INFO {
+	BYTE Reserved1[72];
+	ULONG SessionId;
+	BYTE Reserved2[4];
+	FILETIME ConnectTime;
+	FILETIME DisconnectTime;
+	FILETIME LastInputTime;
+	FILETIME LoginTime;
+	BYTE Reserved3[1096];
+	FILETIME CurrentTime;
+} WINSTATION_INFO;
+
+
 DiskIOCounterInfo *disk_io_counters(void) {
 	DISK_PERFORMANCE_WIN_2008 diskPerformance;
 	DWORD dwSize;
@@ -80,4 +93,134 @@ void free_disk_iocounter_info(DiskIOCounterInfo *tmp)
 {
 	free(tmp->iocounters);
 	free(tmp);
+}
+
+static char *get_drive_type(int type) {
+	switch (type) {
+	case DRIVE_FIXED:
+		return "fixed";
+	case DRIVE_CDROM:
+		return "cdrom";
+	case DRIVE_REMOVABLE:
+		return "removable";
+	case DRIVE_UNKNOWN:
+		return "unknown";
+	case DRIVE_NO_ROOT_DIR:
+		return "unmounted";
+	case DRIVE_REMOTE:
+		return "remote";
+	case DRIVE_RAMDISK:
+		return "ramdisk";
+	default:
+		return "?";
+	}
+}
+
+DiskPartitionInfo *disk_partitions(bool all) {
+	DWORD num_bytes;
+	char *drive_letter = NULL;
+	int type;
+	int _ret;
+	unsigned int old_mode = 0;
+	char *tmpstring;
+	LPTSTR fs_type[MAX_PATH + 1] = { 0 };
+	DWORD pflags = 0;
+	char opts[20];
+	DiskPartitionInfo *ret = NULL; 
+	ret = (DiskPartitionInfo *)calloc(1,sizeof(DiskPartitionInfo));
+	check_mem(ret);
+	ret->nitems = 0;
+	drive_letter = (char *)calloc(1, 255 * sizeof(char));
+	check_mem(drive_letter);
+	// avoid to visualize a message box in case something goes wrong
+	// see https://github.com/giampaolo/psutil/issues/264
+	old_mode = SetErrorMode(SEM_FAILCRITICALERRORS);
+	num_bytes = GetLogicalDriveStrings(254, drive_letter);
+	check(num_bytes, "Error in getting valid drives");
+	while (*drive_letter != '\0') {
+		opts[0] = 0;
+		fs_type[0] = 0;
+		ret->nitems++;
+		ret->partitions = (DiskPartition *)realloc(ret->partitions, ret->nitems * sizeof(DiskPartition));
+		type = GetDriveType(drive_letter);
+
+			// by default we only show hard drives and cd-roms
+		if (all==0) {
+			if ((type == DRIVE_UNKNOWN) ||
+				(type == DRIVE_NO_ROOT_DIR) ||
+				(type == DRIVE_REMOTE) ||
+				(type == DRIVE_RAMDISK)) {
+				goto next;
+			}
+			// floppy disk: skip it by default as it introduces a
+			// considerable slowdown.
+			if ((type == DRIVE_REMOVABLE) &&
+				(strcmp(drive_letter, "A:\\") == 0)) {
+				goto next;
+			}
+		}
+		_ret = GetVolumeInformation(
+			(LPCTSTR)drive_letter, NULL, _ARRAYSIZE(drive_letter),
+			NULL, NULL, &pflags, (LPTSTR)fs_type, _ARRAYSIZE(fs_type));
+		if (_ret == 0) {
+			// We might get here in case of a floppy hard drive, in
+			// which case the error is (21, "device not ready").
+			// Let's pretend it didn't happen as we already have
+			// the drive name and type ('removable').
+			strcat_s(opts, _countof(opts), "");
+			SetLastError(0);
+		}
+		else {
+			if (pflags & FILE_READ_ONLY_VOLUME)
+				strcat_s(opts, _countof(opts), "ro");
+			else
+				strcat_s(opts, _countof(opts), "rw");
+			if (pflags & FILE_VOLUME_IS_COMPRESSED)
+				strcat_s(opts, _countof(opts), ",compressed");
+		}
+
+		if (strlen(opts) > 0)
+			strcat_s(opts, _countof(opts), ",");
+		strcat_s(opts, _countof(opts), get_drive_type(type));
+		tmpstring = (char *)calloc(1, 20 * sizeof(char));
+		check_mem(tmpstring);
+		strcpy(tmpstring, opts);
+		ret->partitions[ret->nitems - 1].opts = tmpstring;
+		tmpstring = (char *)calloc(1, (MAX_PATH + 1) * sizeof(char));
+		check_mem(tmpstring);
+		strcpy(tmpstring, (char *)fs_type);
+		ret->partitions[ret->nitems - 1].device = drive_letter;
+		ret->partitions[ret->nitems - 1].mountpoint = drive_letter;
+		ret->partitions[ret->nitems - 1].fstype = tmpstring;
+		goto next;
+
+	next:
+		drive_letter = strchr(drive_letter, 0) + 1;
+	}
+	SetErrorMode(old_mode);
+	return ret;
+
+error:
+	SetErrorMode(old_mode);
+	if (drive_letter)
+	{
+		free(drive_letter);
+	}
+	if (ret)
+	{
+		free_disk_partition_info(ret);
+	}
+	return NULL;
+}
+void free_disk_partition_info(DiskPartitionInfo *p)
+{
+	uint32_t i;
+	free(p->partitions[0].device);
+	for ( i = 0; i < p->nitems; i++)
+	{
+		free(p->partitions[i].fstype);
+		free(p->partitions[i].opts);
+	}
+	free(p->partitions);
+	free(p);
 }
